95c867c1ba7807f3f5950d387eb3bbffb0f67f82
osakanara <osakanarakyotonagoyatokyo@gmail.com>
2019-06-09 21:20:59 +0900

    concat method

--- test_opencv.py
+++ test_opencv.py
@@ -52,11 +52,40 @@ def resize_photo(resize_img, width, height, interpolation):
     else:
         resized_img = cv2.resize(resize_img, (width, height), interpolation=cv2.INTER_LANCZOS4)
     # 経過時間
-    elapsed_time = time.time() - start_time
-    print(interpolation + "_time:{0}".format(elapsed_time) + "[sec]")
+    resize_time = time.time() - start_time
+    print(interpolation + "_resize_time:{0}".format(resize_time) + "[sec]")
 
     # 画像の保存
     cv2.imwrite('resized.jpg', resized_img)
+
+
+"""
+関数：画像の（縦）連結
+連結する画像：concat_img
+連結回数：count
+"""
+def concat_photo(in_img, count):
+
+    # 処理開始時間
+    start_time = time.time()
+    # 画像の読込
+    img = cv2.imread(in_img)
+    # 連結回数分の配列を作成
+    concat_list = []
+    for _ in range(count):
+        concat_list.append(img)
+
+    # 画像の連結
+    concat_img = cv2.vconcat(concat_list)
+
+    # 経過時間
+    concat_time = time.time() - start_time
+    print("concat_time:{0}".format(concat_time) + "[sec]")
+
+    # 画像の保存
+    cv2.imwrite('concat.jpg', concat_img)
+
+
 
 
 # メイン処理
@@ -65,9 +94,13 @@ if __name__== '__main__':
     add_img = 'add.jpg'
     back_img = 'back.jpg'
     resize_img = 'resize.jpg'
+    lena_img = 'lena.jpg'
 
     # 画像の合成　呼出
     composite_phto(add_img=add_img, back_img=back_img)
 
     # 画像のリサイズ
     resize_photo(resize_img=resize_img, width=200, height=200, interpolation='linear')
+
+    # 画像の連結　１０回
+    concat_photo(in_img=lena_img, count=10)
