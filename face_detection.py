d65794f590859bdbbf503f6f16f893c066c876ea
osakanara <osakanarakyotonagoyatokyo@gmail.com>
2019-06-10 19:02:19 +0900

    顔検出

---
+++ face_detection.py
@@ -0,0 +1,23 @@
+import cv2
+
+# 識別器の読み込み
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# 検出対象の読み込み
+img = cv2.imread('sample.jpg')
+# グレースケールの読み込み
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray)
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
